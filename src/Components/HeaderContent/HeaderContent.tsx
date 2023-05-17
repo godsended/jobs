2ba974860f31206838bf78ed88594a4846546ab4
Ilya Kolesnikov <kolesnikovila6@gmail.com>
2023-05-17 17:21:15 +0300

    Current location highlight

--- src/Components/HeaderContent/HeaderContent.tsx
+++ src/Components/HeaderContent/HeaderContent.tsx
@@ -1,14 +1,25 @@
 import React from "react";
-import {Center, Flex, Image, Space, Text} from "@mantine/core";
+import {Anchor, Center, Flex, Image, Space, Text} from "@mantine/core";
 import "./HeaderContent.css"
-import {NavLink} from "react-router-dom";
+import {NavLink, useLocation} from "react-router-dom";
 
 function HeaderContent() {
+    let location = useLocation();
     return (
         <Flex gap={"xl"}
               justify={{sm: "space-between"}}>
-            <Center><NavLink to={"/"}>Поиск вакансий</NavLink></Center>
-            <Center>Избранное</Center>
+            <Center>
+                <Anchor underline={false} color={location.pathname === "/" ? "blue" : "black"}>
+                    <NavLink to={"/"} style={{color: 'inherit', textDecoration: 'inherit'}}>
+                        Поиск вакансий
+                    </NavLink>
+                </Anchor>
+            </Center>
+            <Anchor underline={false} color={location.pathname === "/featured" ? "blue" : "black"}>
+                <NavLink to={"/featured"} style={{color: 'inherit', textDecoration: 'inherit'}}>
+                    Избранное
+                </NavLink>
+            </Anchor>
         </Flex>
     )
 }
